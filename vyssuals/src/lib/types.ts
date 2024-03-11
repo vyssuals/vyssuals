c11ece42f5ae4650efb608cb496597d034e4919c
Yskert Schindel <55532939+yschindel@users.noreply.github.com>
2024-03-11 15:18:45 +0100

    Refactor types.ts file

--- vyssuals/src/lib/types.ts
+++ vyssuals/src/lib/types.ts
@@ -15,7 +15,6 @@ export interface ChartData {
     unitSymbol: string;
 }
 
-
 export interface WebSocketMessage {
     type: string; // The type of the message, either 'data' or 'disconnect'
     timestamp: string; // The timestamp of the message sent by the server
@@ -77,91 +76,10 @@ export interface Attributes {
     [key: string]: string | number; // Key is the Column Name, Value is the Column Value
 }
 
-// export type VisibleItems = [];
-
-// export class Updates {
-//   private dict: { [timestamp: string]: number } = {};
-//   private list: VisibleItems[] = [];
-
-//   addUpdate(timestamp: string, update: VisibleItems): void {
-//     this.dict[timestamp] = this.list.length;
-//     this.list.push(update);
-//   }
-
-//   getUpdate(timestamp: string): VisibleItems {
-//     const index = this.dict[timestamp];
-//     return this.list[index];
-//   }
-
-//   getLastUpdate(): VisibleItems {
-//     return this.list[this.list.length - 1];
-//   }
-// }
-
-// export class DataSource {
-//   data: {
-//     [key: string]: Item; // Key is the Item ID, Value is the Item
-//   };
-//   metadata: Metadata;
-//   lastUpdate: string; // used for filtering items included in the last update
-//   updates: Updates;
-//   type: "file" | "websocket";
-//   file?: File;
-//   name: string;
-
-//   constructor(type: "file" | "websocket", lastUpdate: string, name: string) {
-//     this.data = {};
-//     this.metadata = {};
-//     this.lastUpdate = lastUpdate;
-//     this.updates = new Updates();
-//     this.type = type;
-//     this.file = undefined;
-//     this.name = name;
-//   }
-// }
-
-// export interface Store {
-//   dataSources: {
-//     [key: string]: DataSource; // Key is the DataSource Name, Value is the DataSource
-//   };
-// }
-
-// export class Item {
-//   id: string;
-//   private dict: { [timestamp: string]: number } = {};
-//   private list: Attributes[] = [];
-
-//   constructor(id: string);
-//   constructor(id: string, timestamp: string, version: Attributes);
-//   constructor(id: string, timestamp?: string, version?: Attributes) {
-//     this.id = id;
-//     if (timestamp && version) {
-//       this.addUpdate(timestamp, version);
-//     }
-//   }
-
-//   addUpdate(timestamp: string, version: Attributes): void {
-//     this.dict[timestamp] = this.list.length;
-//     this.list.push(this.setCountAndTimestamp(version, timestamp));
-//   }
-
-//   getUpdate(timestamp: string): Attributes {
-//     const index = this.dict[timestamp];
-//     return this.list[index];
-//   }
-
-//   getLastUpdate(): Attributes {
-//     return this.list[this.list.length - 1];
-//   }
-
-//   setCountAndTimestamp(attributes: Attributes, timestamp: string): Attributes {
-//     return {
-//       ...attributes,
-//       count: 1,
-//       timestamp: timestamp,
-//     };
-//   }
-// }
+export interface Info {
+    key: string;
+    value: string;
+}
 
 export const COLUMN_TYPES = ["string", "number"];
 export type ChartType = "line" | "bar" | "doughnut" | "total";
