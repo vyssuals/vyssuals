67265894b2ed2f90fbdccd62fa73c9f40987726e
Yskert Schindel <55532939+yschindel@users.noreply.github.com>
2024-03-12 10:14:53 +0100

    Refactor WebSocket client code to improve message serialization

--- vyssuals/src/lib/tests/ws_client.py
+++ vyssuals/src/lib/tests/ws_client.py
@@ -1,4 +1,4 @@
-import asyncio
+import sys
 import websockets
 import json
 from classes import ComplexEncoder
@@ -13,7 +13,14 @@ async def send_data():
             # Wait for the user to press enter
             input("Press enter to send data: ")
             dummy_data = generate_dummy_data()
-            await websocket.send(json.dumps(dummy_data, cls=ComplexEncoder))
+            jstring = json.dumps(dummy_data, cls=ComplexEncoder)
+            # Serialize the message to JSON
+            message_json = json.dumps(dummy_data, default=lambda o: o.__dict__)
+
+            # Print the size of the serialized message
+            print(sys.getsizeof(message_json))
+            
+            await websocket.send(jstring)
             
 # Start the client
 asyncio.run(send_data())
