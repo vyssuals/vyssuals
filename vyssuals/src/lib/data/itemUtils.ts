2a944e71e610c851c9fb95febf1b8c581ffe4a46
Yskert Schindel <55532939+yschindel@users.noreply.github.com>
2024-03-18 13:54:33 +0100

    Refactor itemUtils.ts to include timestamp parameter in getItemAttributes and getItemValue functions

--- vyssuals/src/lib/data/itemUtils.ts
+++ vyssuals/src/lib/data/itemUtils.ts
@@ -1,14 +1,42 @@
 import type { Attributes, Item, Versions } from "../types";
 import Dexie from "dexie";
 
-export function getLatestItemAttributes(versions: Versions): Attributes {
+export function getItemAttributes(versions: Versions, timestamp: string = ""): Attributes {
+    if (timestamp != "") {
+        if (timestamp in versions) { 
+            console.log(`found timestamp: ${timestamp}, returning`)
+            return versions[timestamp]; }
+        const timestamps: string[] = Object.keys(versions).sort().reverse();
+        for (const ts of timestamps) {
+            console.log(`ts: ${ts}, timestamp: ${timestamp}`)
+            if (ts <= timestamp) {
+                console.log(`returning ${ts}`)
+                console.log(versions[ts])
+                return versions[ts];
+            }
+        }
+        return {} as Attributes;
+    }
     // sort item.versions by timestamp and return the values of the latest version
     const timestamps: string[] = Object.keys(versions).sort().reverse();
     const latestVersion = versions[timestamps[0]];
     return latestVersion;
 }
 
-export function getLatestItemValue(versions: Versions, key: string): string | number {
+export function getItemValue(versions: Versions, key: string, timestamp: string = ""): string | number {
+    if (timestamp != "") {
+        // try to get the value from the specified timestamp
+        if (timestamp in versions) { return versions[timestamp][key] ?? ""; }
+        // if the timestamp is not in versions, get the values from a version prior to the specified timestamp
+        const timestamps: string[] = Object.keys(versions).sort().reverse();
+        for (const ts of timestamps) {
+            if (ts <= timestamp) {
+                return versions[ts][key] ?? "";
+            }
+        }
+        return "";
+    }
+
     const versionCount = Object.keys(versions).length;
     if (versionCount === 0) {
         return "";
