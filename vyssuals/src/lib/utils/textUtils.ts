7dbd212284a78da9bddd82a5816125d4da00e166
Yskert Schindel <55532939+yschindel@users.noreply.github.com>
2024-03-19 07:40:24 +0100

    Add formatSubtitle function to textUtils.ts

--- vyssuals/src/lib/utils/textUtils.ts
+++ vyssuals/src/lib/utils/textUtils.ts
@@ -10,3 +10,19 @@ export function titleCase(str: string): string {
         return word.replace(word[0], word[0].toUpperCase());
     }).join(' ');
 }
+
+export function formatSubtitle(chartConfig: ChartConfig, unitSymbol: string): string {
+    let update: string = chartConfig.update;
+    if (chartConfig.update != "Latest Update") {
+        update = new Date(chartConfig.update).toLocaleString();
+    }
+    return `${truncateString(chartConfig.dataSourceName)} // ${truncateString(update)} // ${unitSymbol}`;
+}
+
+function truncateString(str: string,): string {
+    const num = 20;
+    if (str.length <= num) {
+        return str;
+    }
+    return str.slice(0, num) + '...';
+}
