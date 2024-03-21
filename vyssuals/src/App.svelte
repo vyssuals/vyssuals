8654fb62df2e4d423f7c98de34ce0c5dfa823e64
Yskert Schindel <55532939+yschindel@users.noreply.github.com>
2024-03-21 09:06:55 +0100

    Update tutorial link in footer

--- vyssuals/src/App.svelte
+++ vyssuals/src/App.svelte
@@ -74,7 +74,7 @@
     <a class="info-text bottom-right" href="https://www.yssentyl.com" target="_blank">© Yssentyl 2024</a>
     <span class="bottom-left">
         <button class="info-text" on:click={() => handleDataInfo()}>My Data?</button>
-        <a class="info-text" href="https://www.yssentyl.com" target="_blank">Tutorial?</a>    
+        <a class="info-text" href="https://yssentyl.com/blog/csv-data-visualization-in-under-10-seconds" target="_blank">Tutorial?</a>    
     </span>
 </footer>
 
